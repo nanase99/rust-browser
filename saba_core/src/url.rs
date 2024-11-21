45cdee77b2b22d294c7aafed12aebd47e1f3a351
nanase <sgmdq601@gmail.com>
2024-11-21 22:33:05 +0900

    add: is_http

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -23,6 +23,18 @@ impl Url {
 
 impl Url {
     pub fn parse(&mut self) -> Result<Self, String> {
-        unimplemented!()
+        if self.is_http() {
+            return Err("Only HTTP scheme is supported".to_string());
+        }
+        unimplemented!();
+    }
+}
+
+impl Url {
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
     }
 }
