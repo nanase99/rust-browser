8f316a602f79abf48b9836995d8573d246025b48
nanase <sgmdq601@gmail.com>
2024-12-01 20:03:13 +0900

    feat: HttpClient作成

--- net/wasabi/src/http.rs
+++ net/wasabi/src/http.rs
@@ -1,6 +1,9 @@
 extern crate alloc;
 
-use alloc::string::String;
+use alloc::format;
+use alloc::string::{String, ToString};
+use alloc::vec::Vec;
+use noli::net::{lookup_host, SocketAddr, TcpStream};
 use saba_core::error::Error;
 use saba_core::http::HttpResponse;
 
@@ -12,6 +15,78 @@ impl HttpClient {
     }
 
     pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
-        !unimplemented!()
+        // IPアドレスの取得
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP addresses: {:#?}",
+                    e
+                )))
+            }
+        };
+
+        if ips.is_empty() {
+            return Err(Error::Network("Failed to find IP addresses".to_string()));
+        }
+
+        let socket_addr: SocketAddr = (ips[0], port).into();
+
+        // ストリームの構築
+        let mut stream = match TcpStream::connect(socket_addr) {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to connect to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        // リクエストラインの構築
+        let mut request = String::from("GET /");
+        request.push_str(&path);
+        request.push_str("HTTP/1.1\n");
+
+        // ヘッダの追加
+        request.push_str("Host: ");
+        request.push_str(&host);
+        request.push('\n');
+        request.push_str("Accept: text/html\n");
+        request.push_str("Connection: close\n");
+        request.push('\n');
+
+        // リクエストの送信
+        let _bytes_written = match stream.write(request.as_bytes()) {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(Error::Network(
+                    "Failed to request to TCP stream".to_string(),
+                ))
+            }
+        };
+
+        // レスポンスの受信
+        let mut received = Vec::new();
+        loop {
+            let mut buf = [0u8; 4096];
+            let bytes_read = match stream.read(&mut buf) {
+                Ok(bytes) => bytes,
+                Err(_) => {
+                    return Err(Error::Network(
+                        "Failed to receive a request from TCP stream".to_string(),
+                    ))
+                }
+            };
+            if bytes_read == 0 {
+                break;
+            }
+            received.extend_from_slice(&buf[..bytes_read]);
+        }
+
+        // HTTPレスポンスの構築
+        match core::str::from_utf8(&received) {
+            Ok(response) => HttpResponse::new(response.to_string()),
+            Err(e) => Err(Error::Network(format!("Invalid received response: {}", e))),
+        }
     }
 }
