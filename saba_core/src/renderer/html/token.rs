66ccb04590f891e9507df64c2844895dd9ada120
nanase <sgmdq601@gmail.com>
2024-12-09 15:18:24 +0900

    add: HtmlToken構造体を作成

---
+++ saba_core/src/renderer/html/token.rs
@@ -0,0 +1,22 @@
+use crate::renderer::html::attribute::Attribute;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    // 開始タグ
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    // 終了タグ
+    EndTag {
+        tag: String,
+    },
+    // 文字
+    Char(char),
+    // End of File
+    Eof,
+}
+
