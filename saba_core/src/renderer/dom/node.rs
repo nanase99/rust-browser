897aee2f5a7f1ca22bc6b6bbc4f61ac1d2ed5259
nanase <sgmdq601@gmail.com>
2024-12-20 13:51:21 +0900

    add: Window

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -27,6 +27,10 @@ impl Node {
         }
     }
 
+    pub fn set_window(&mut self, window: Weak<RefCell<Window>>) {
+        self.window = window;
+    }
+
     pub fn set_parent(&mut self, parent: Weak<RefCell<Node>>) {
         self.parent = parent;
     }
@@ -78,3 +82,27 @@ pub enum NodeKind {
     Text(String),
 }
 
+/// https://dom.spec.whatwg.org/multipage/nav-history-apis.html#window
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
+}
