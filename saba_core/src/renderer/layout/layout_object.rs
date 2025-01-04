ec53f12308768e625b923da8991da3504dd41b6e
nanase <sgmdq601@gmail.com>
2025-01-04 23:42:35 +0900

    add: layout_view tests

--- saba_core/src/renderer/layout/layout_object.rs
+++ saba_core/src/renderer/layout/layout_object.rs
@@ -313,6 +313,12 @@ impl LayoutObject {
     }
 }
 
+impl PartialEq for LayoutObject {
+    fn eq(&self, other: &Self) -> bool {
+        self.kind == other.kind
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum LayoutObjectKind {
     Block,
